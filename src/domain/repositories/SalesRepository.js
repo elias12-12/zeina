23796d4b1911405e0d1ff4db81c0c254d035a01d
Elias Saab <saabelias37@gmail.com>
2025-10-31 07:51:45 +0200

    third commit

--- src/domain/repositories/SalesRepository.js
+++ src/domain/repositories/SalesRepository.js
@@ -7,7 +7,7 @@ export class SalesRepository {
         const sql = `
             INSERT INTO sales (user_id, subtotal, discount_percentage, discount_amount, total_amount)
             VALUES ($1, 0, 0, 0, 0)
-            RETURNING sale_id, user_id, sale_date, subtotal, discount_percentage, discount_amount, total_amount;
+            RETURNING sale_id, user_id, TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date, subtotal, discount_percentage, discount_amount, total_amount;
         `;
         const { rows } = await pool.query(sql, [user_id]);
         return new Sales(rows[0]);
@@ -18,7 +18,7 @@ export class SalesRepository {
             UPDATE sales
             SET total_amount = $1
             WHERE sale_id = $2
-            RETURNING sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, sale_date;
+            RETURNING sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date;
         `;
         const { rows } = await pool.query(sql, [total_amount, sale_id]);
         return rows[0] ? new Sales(rows[0]) : null;
@@ -32,26 +32,26 @@ export class SalesRepository {
             UPDATE sales
             SET subtotal = $1, discount_percentage = $2, discount_amount = $3, total_amount = $4
             WHERE sale_id = $5
-            RETURNING sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, sale_date;
+            RETURNING sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount,TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date;
         `;
         const { rows } = await pool.query(sql, [subtotal, discount_percentage, discount_amount, total_amount, sale_id]);
         return rows[0] ? new Sales(rows[0]) : null;
     }
 
       async findAll() {
-        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, sale_date FROM sales ORDER BY sale_id DESC;`;
+        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount,TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date FROM sales ORDER BY sale_id DESC;`;
         const { rows } = await pool.query(sql);
         return rows.map(r => new Sales(r));
     }
 
   async findById(sale_id) {
-        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, sale_date FROM sales WHERE sale_id = $1;`;
+        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date FROM sales WHERE sale_id = $1;`;
         const { rows } = await pool.query(sql, [sale_id]);
         return rows[0] ? new Sales(rows[0]) : null;
     }
 
       async findByCustomer(user_id) {
-        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, sale_date FROM sales WHERE user_id = $1 ORDER BY sale_id DESC;`;
+        const sql = `SELECT sale_id, user_id, subtotal, discount_percentage, discount_amount, total_amount, TO_CHAR(sale_date, 'DD/MM/YYYY') as sale_date FROM sales WHERE user_id = $1 ORDER BY sale_id DESC;`;
         const { rows } = await pool.query(sql, [user_id]);
         return rows.map(r => new Sales(r));
     }
